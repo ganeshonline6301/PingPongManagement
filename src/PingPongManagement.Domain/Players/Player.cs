6dc0913b9f455c02e655bef7bb9134e332f05db4
Ganesh Sri Surya <robinganesh007@gmail.com>
2025-01-06 11:13:51 +0530

    Player entity created

---
+++ src/PingPongManagement.Domain/Players/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace PingPongManagement.Domain.Players;
+
+public class Player
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public Guid UserId { get; set; }
+}
